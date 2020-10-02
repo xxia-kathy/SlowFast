95a4c4e0d923feb4c104537512ec9a5787af8f17
xiaxia <kathyxxj@service-inf-drzs-vpn-070.ethz.ch>
2020-10-02 12:13:24 +0200

    ks = 1,2

--- slowfast/utils/meters.py
+++ slowfast/utils/meters.py
@@ -361,7 +361,7 @@ class TestMeter(object):
         self.data_timer.pause()
         self.net_timer.reset()
 
-    def finalize_metrics(self, ks=(1, 5)):
+    def finalize_metrics(self, ks=(1, 2)):
         """
         Calculate and log the final ensembled metrics.
         ks (tuple): list of top-k values for topk_accuracies. For example,
